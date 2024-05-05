c500fdd990e6d1720ebf707a0beb156e0f9a6810
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-04 18:17:24 -0700

    get form data

---
+++ app/lib/actions.ts
@@ -0,0 +1,12 @@
+//mark all the exported functions within the file as server functions
+'use server';
+ 
+export async function createInvoice(formData: FormData) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // Test it out:
+  console.log(rawFormData);
+}
